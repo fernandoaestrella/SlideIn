60c4bb2f48e1f196b1b31c30becf6597de2067f1
Fernando Estrella <fernandoaestrella@gmail.com>
2019-06-25 03:41:20 -0400

    First commit

---
+++ Assets/Tile.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile : MonoBehaviour
+{
+    public bool isTraversable;
+    public Unit unit;
+    public Modifier modifier;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
