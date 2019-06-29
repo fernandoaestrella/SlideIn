bd2934767bdc0973df1f19a7237f9a12f95fb9a7
Fernando Estrella <fernandoaestrella@gmail.com>
2019-06-29 03:11:53 -0400

    Scores and remaining time displayed

--- Assets/MainMenu.cs
+++ Assets/MainMenu.cs
@@ -12,7 +12,6 @@ public class MainMenu : MonoBehaviour
 
     public void Quit()
     {
-        print("i quit");
         Application.Quit();
     }
 }
---
+++ Assets/PlayerScoreDisplay.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerScoreDisplay : MonoBehaviour
+{
+    public GameManager game;
+    public Text scoreText;
+
+    // Update is called once per frame
+    void Update()
+    {
+        scoreText.text = "Player 1 Score: " + ((Player)game.players[0]).score + "\n\n\nPlayer 2 Score: " + ((Player)game.players[1]).score;
+    }
+}
